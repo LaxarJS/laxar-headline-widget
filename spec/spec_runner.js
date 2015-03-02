c3e1cbcd64088c3e7b7570388cea9faae6adcefd
Jonas Schulte <jonas.schulte@aixigo.de>
2015-03-02 15:15:05 +0100

    Initial opensource version (2.0.0-pre).

---
+++ spec/spec_runner.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2015 aixigo AG
+ * Released under the MIT license.
+ * http://laxarjs.org/license
+ */
+( function( global ) {
+   'use strict';
+   global.laxarSpec = {
+      title: 'AxHeadlineWidget Specification',
+      tests: [
+         'ax-headline-widget.spec'
+      ],
+      requireConfig: {}
+   };
+} )( this );
